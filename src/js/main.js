e5cfe520672d821770c0cd47055d178a1cf3f606
Ognjen <ognjen@cloudhorizon.com>
2015-11-26 19:57:39 +0100

    feat(home): Add desktop size style, add animation for the homepage banner

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,34 @@ $('.menu-button, .overlay').on('click', function() {
 function toggleMenu() {
 	$('.menu, html, .overlay').toggleClass('menu-open');	
 }
+
+var scrollSpeed = 40;
+  
+// set the default position
+var current = 0;
+
+function bgscroll(){
+
+  // move the background with backgrond-position css properties
+  if($('.home-wrapper .banner').hasClass('swipe')) {
+    current += 40;
+    $('.home-wrapper .banner').css("backgroundPosition", current+"px bottom");
+  } else {
+    current += 1;
+    $('.home-wrapper .banner').css("backgroundPosition", current+"px bottom");
+  }
+    
+
+}
+
+$('.home-wrapper .banner').on('click', function() {
+  $(this).addClass('swipe');
+  setTimeout(function(){
+    $('.home-wrapper .banner').removeClass('swipe'); 
+  }, 300)
+})
+
+//Calls the scrolling function repeatedly
+if($('.home-wrapper').length) {
+  setInterval(bgscroll, scrollSpeed);
+}
